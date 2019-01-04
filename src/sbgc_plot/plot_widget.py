4005c9ab44a66e982ad043c295f669111c9f26d2
bolatashim <bolatashim>
2019-01-04 10:46:30 +0900

    changes are starting

--- src/sbgc_plot/plot_widget.py
+++ src/sbgc_plot/plot_widget.py
@@ -1,35 +1,3 @@
-#!/usr/bin/env python
-
-# Copyright (c) 2011, Dorian Scholz, TU Darmstadt
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#   * Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-#   * Redistributions in binary form must reproduce the above
-#     copyright notice, this list of conditions and the following
-#     disclaimer in the documentation and/or other materials provided
-#     with the distribution.
-#   * Neither the name of the TU Darmstadt nor the names of its
-#     contributors may be used to endorse or promote products derived
-#     from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 import os
 import rospkg
 import roslib
@@ -116,11 +84,13 @@ def is_plottable(topic_name):
 class PlotWidget(QWidget):
     _redraw_interval = 40
 
-    def __init__(self, initial_topics=None, start_paused=False):
+    def __init__(self, initial_topics = ["/gimbal/pitch/data", "/gimbal/yaw/data"], start_paused=False):
+        
+        print("Iniitial topics have been set, starting...\n")
         super(PlotWidget, self).__init__()
         self.setObjectName('PlotWidget')
 
-        self._initial_topics = initial_topics
+        self._initial_topics = ["/gimbal/pitch/data", "/gimbal/yaw/data"]
 
         rp = rospkg.RosPack()
         ui_file = os.path.join(rp.get_path('sbgc_plot'), 'resource', 'plot.ui')
@@ -164,6 +134,7 @@ class PlotWidget(QWidget):
 
         if self._initial_topics:
             for topic_name in self._initial_topics:
+                print("Adding topic", topic_name);
                 self.add_topic(topic_name)
             self._initial_topics = None
         else:
